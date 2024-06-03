9e64c80b70f46feec4bfe382459651715ab8a116
Gareg <reg_ga@farmorereal.com>
2024-06-02 18:58:30 -0700

    added EnemyManager.js

---
+++ src/classes/EnemyManager.js
@@ -0,0 +1,23 @@
+class EnemyManager extends Object
+{
+    constructor()
+    {
+        this.waveClock = new Timer;
+        this.waveClock.
+    }
+
+    waveTimer()
+    {
+
+    }
+
+    spawnWave(playerX, playerY)
+    {
+
+    }
+
+    spawnEnemy(enemyType, x, y)
+    {
+
+    }
+}
