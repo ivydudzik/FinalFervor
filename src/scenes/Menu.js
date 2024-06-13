3b032c7b5d7736e6b83d6f617e7c17a8f123b1e9
Gareg <reg_ga@farmorereal.com>
2024-06-12 22:18:40 -0700

    added simplistic main menu and menu button in battle scene

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -11,9 +11,16 @@ class Menu extends Phaser.Scene {
         this.my.sprite.start = this.add.text(game.config.width / 2,
             game.config.height / 2,
             "Final Fervor",
-            { fontSize: '76px', fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' });
+            { color: '#034600', fontSize: '76px', fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' });
         this.my.sprite.start.setOrigin(0.5, 0.5);
-        this.time.delayedCall(1000, this.nextScene, [], this);
+        //this.time.delayedCall(1000, this.nextScene, [], this);
+        const credits = this.add.text(20, 840, 'A game by: Ivy Dudzik & Gabe Ahrens', { fill: '#000', fontSize: '36px', fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' });
+        const startButton = this.add.text(game.config.width / 2, 600, 'Start', { fill: '#6AC965', fontSize: '48px', fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' });
+        startButton.setOrigin(0.5, 0.5);
+        startButton.setInteractive();
+        startButton.on('pointerover', () => { startButton.setFill('#85E740'); });
+        startButton.on('pointerout', () => { startButton.setFill('#6AC965'); });
+        startButton.on('pointerdown', () => { this.nextScene(); });
     }
 
     nextScene() {
